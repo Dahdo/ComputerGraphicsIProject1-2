2b4847b8fc05015e60dbff7676afb7590f41d4a5
dahdo <dndashimye15@gmail.com>
2024-02-28 03:02:54 +0000

    backup

---
+++ Converters.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace ComputerGraphicsIProject
+{
+    public class BitmapToBitmapImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is System.Drawing.Bitmap bitmap)
+            {
+                BitmapImage bitmapImage = new BitmapImage();
+
+                // Convert System.Drawing.Bitmap to BitmapImage
+                var memoryStream = new System.IO.MemoryStream();
+                bitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                memoryStream.Position = 0;
+
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memoryStream;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Win32;
+using System.ComponentModel;
+using System.Drawing;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+
+namespace ComputerGraphicsIProject
+{
+
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private Bitmap? _imageSource;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public Bitmap? ImageSource
+        {
+            get { return _imageSource; }
+            set
+            {
+                if (_imageSource != value)
+                {
+                    _imageSource = value;
+                    OnPropertyChanged(nameof(ImageSource));
+                }
+            }
+        }
+
+        private System.Windows.Controls.Image? refImage;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            refImage = FindName("RefImage") as System.Windows.Controls.Image;
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Open Image File",
+                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp|All Files|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+
+                try
+                {
+                    Bitmap? bitmap = new Bitmap(selectedFilePath);
+                    ImageSource = bitmap;
+
+                    if (refImage != null)
+                    {
+                        BitmapImage refBitmapImage = new BitmapImage(new Uri(selectedFilePath));
+                        refImage.Source = refBitmapImage;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading image: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ImageSource = ImageProcessor.ApplyInversionFilter(ImageSource);
+        }
+    }
+}
