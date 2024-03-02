e68e79c633a69bc11ae4201db027080e76e3610d
dahdo <dndashimye15@gmail.com>
2024-03-02 00:21:09 +0000

    improved implementation of inversion

--- FunctionalFilters.cs
+++ FunctionalFilters.cs
@@ -10,35 +10,38 @@ namespace ComputerGraphicsIProject
     {
         public static void ApplyInversionFilter(Bitmap? inputBitmap)
         {
-            if(inputBitmap == null)
+            /*
+             * This implementation makes use these 2 resources to create an improved version
+             * https://www.codeproject.com/Articles/1989/Image-Processing-for-Dummies-with-C-and-GDI-Part-1
+             * https://learn.microsoft.com/en-us/dotnet/api/system.drawing.imaging.bitmapdata.scan0?view=dotnet-plat-ext-8.0#system-drawing-imaging-bitmapdata-scan0
+             */
+            if (inputBitmap == null)
                 throw new ArgumentNullException("input");
 
             BitmapData bitmapData = inputBitmap.LockBits(new Rectangle(0, 0, inputBitmap.Width, 
                 inputBitmap.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
 
-
+            // Get the number of bytes in a stride
             int stride = bitmapData.Stride;
-            int memOffset = stride - inputBitmap.Width * 3;
-            int numBytesWidth = inputBitmap.Width * 3;
-;
 
             try
             {
                 unsafe
                 {
+                    
                     // Pointer to the first byte of the pixel data
                     byte* bitmapDataPtr = (byte*)bitmapData.Scan0;
 
                     for (int y = 0; y < inputBitmap.Height; ++y)
                     {
-                        for (int x = 0; x < numBytesWidth; x += 3)
+                        for (int x = 0; x < stride; x += 3)
                         {
-                            bitmapDataPtr[0] = (byte)(255 - bitmapDataPtr[0]); 
-                            bitmapDataPtr[1] = (byte)(255 - bitmapDataPtr[1]);
-                            bitmapDataPtr[2] = (byte)(255 - bitmapDataPtr[2]);
-                            bitmapDataPtr += 3;
+                            bitmapDataPtr[0] = (byte)(255 - bitmapDataPtr[0]); // Red channel
+                            bitmapDataPtr[1] = (byte)(255 - bitmapDataPtr[1]); // Green channel
+                            bitmapDataPtr[2] = (byte)(255 - bitmapDataPtr[2]); // Blue channel
+
+                            bitmapDataPtr += 3; // Jump to the next pixel
                         }
-                        bitmapDataPtr += memOffset;
                     }
                 }
             }
@@ -46,7 +49,6 @@ namespace ComputerGraphicsIProject
             {
                 inputBitmap.UnlockBits(bitmapData);
             }
-            // inputBitmap.Save("C:\\Users\\Dahdo\\source\\repos\\ComputerGraphicsIProject\\test.png", ImageFormat.Png);
         }
 
     }
